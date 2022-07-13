fbc1a00d54cd1d0cbc105a516c70815cc95f27ce
pksPriyankSharma <41829036+pksPriyankSharma@users.noreply.github.com>
2022-07-14 00:50:03 +0530

    commit post creating the testuser branch

--- helloworld.py
+++ helloworld.py
@@ -3,7 +3,11 @@
 print("Hello World")
 print("I am learning git")
 print("testing the git hub command, making the changes")
+<<<<<<< HEAD
 print("making changes post creating another user directory under use_cases")
 print("adding more lines post cloning to other user")
+=======
+print("testing the merge operation")
+>>>>>>> 24247d1... testing the merge
 
 
